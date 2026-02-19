67315f32880afe41300ab42bddc97601057c985a
javeria2108 <javeriazh@gmail.com>
2026-02-19 21:41:27 +0500

    adding a user

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,8 +1,30 @@
 import { RequestHandler } from "express";
 import { prisma } from "../lib/prisma";
+import bcrypt from "bcryptjs";
+
 const registerUser: RequestHandler = async (req, res) => {
   const { name, email, password } = req.body;
   const userExists = await prisma.user.findUnique({ where: { email } });
+  if (userExists) {
+    return res
+      .status(400)
+      .json({ message: "User with this email already exists" });
+  }
+
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Create user
+  const user = await prisma.user.create({
+    data: {
+      username: name,
+      email: email,
+      passwordHash: hashedPassword,
+    }
+  })
+
+  res.status(201).json({ message: "User registered successfully", user });
 };
 
 export { registerUser };
