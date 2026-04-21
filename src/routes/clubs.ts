5bb641f9493ef3d1efde15f3135a82e9fa37a3ff
javeria2108 <javeriazh@gmail.com>
2026-04-21 15:21:32 +0500

    add getClubById route

--- src/routes/clubs.ts
+++ src/routes/clubs.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import { createClub, getClubs } from "../controllers/clubController";
+import {
+  createClub,
+  getClubById,
+  getClubs,
+} from "../controllers/clubController";
 import { get } from "node:http";
 
 const router = express.Router();
 
 router.get("/", getClubs);
 router.post("/", createClub);
+router.get("/:id", getClubById);
 
 export default router;
