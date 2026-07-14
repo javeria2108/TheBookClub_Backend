bf9b174326bc9e090ecd20a00a8e7226d308e1fd
javeria2108 <javeriazh@gmail.com>
2026-07-14 16:20:19 +0500

    chore(repo): stop tracking generated backend files

--- dist/types/bookClub.types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * BookClub-related TypeScript types inferred from Zod schemas.
- * The schemas remain the runtime source of truth.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
--- dist/utils/validation.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFirstValidationMessage = getFirstValidationMessage;
-function getFirstValidationMessage(error, fallback = "Invalid request data") {
-    return error.issues[0]?.message ?? fallback;
-}
